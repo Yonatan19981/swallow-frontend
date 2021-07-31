f366cfcc03798af0abd345953c98ad2bd721b7a4
YONATAN10111 <60484385+YONATAN10111@users.noreply.github.com>
2021-07-31 14:18:30 +0300

    updates

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,4 +1,6 @@
 import Web3 from "web3";
+import Web3Modal from "web3modal";
+
 // require('dotenv').config();
 // const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
@@ -7,39 +9,24 @@ import Web3 from "web3";
 const FALLBACK_WEB3_PROVIDER = process.env.REACT_APP_NETWORK || 'http://0.0.0.0:8545';
 
 
+const providerOptions = {
+  /* See Provider Options Section */
+};
+
 const getWeb3 = () =>
-  new Promise((resolve, reject) => {
+  new Promise(async (resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Acccounts now exposed
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider(
-          FALLBACK_WEB3_PROVIDER
-        );
-        const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Infura/Local web3.");
-        resolve(web3);
-      }
-    });
+  
+const web3Modal = new Web3Modal({
+  network: "ropsten", // optional
+  cacheProvider: true, // optional
+  providerOptions // required
+});
+
+const provider = await web3Modal.connect();
+
+const web3 = new Web3(provider);
+resolve(web3)
   });
 
 const getGanacheWeb3 = () => {
