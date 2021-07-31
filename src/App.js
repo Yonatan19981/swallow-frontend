940ee1149921c95aa443b164f20e59ea45d6bcec
YONATAN10111 <60484385+YONATAN10111@users.noreply.github.com>
2021-07-31 13:27:21 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,50 +1,118 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import getWeb3, { getGanacheWeb3, Web3 } from "./utils/getWeb3";
+import Header from "./components/Header/index.js";
+import Footer from "./components/Footer/index.js";
+import Publish from "./components/Publish/index.js";
+import MyPhotos from "./components/MyPhotos/index.js";
+import PhotoMarketplace from "./components/PhotoMarketplace/index.js";
+import ipfs from './components/ipfs/ipfsApi.js'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import { Loader, Button, Card, Input, Heading, Table, Form, Flex, Box, Image } from 'rimble-ui';
+import { zeppelinSolidityHotLoaderOptions } from './webpack';
 
-  handleClick = api => e => {
-    e.preventDefault()
+import styles from './App.module.scss';
+//import './App.css';
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
+  constructor(props) {    
+    super(props);
+
+    this.state = {
+      /////// Default state
+      storageValue: 0,
+      web3: null,
+      accounts: null,
+      route: window.location.pathname.replace("/", ""),
+    };
+  }
+ 
+  //////////////////////////////////// 
+  ///// Ganache
+  ////////////////////////////////////
+  getGanacheAddresses = async () => {
+    if (!this.ganacheProvider) {
+      this.ganacheProvider = getGanacheWeb3();
+    }
+    if (this.ganacheProvider) {
+      return await this.ganacheProvider.eth.getAccounts();
+    }
+    return [];
+  }
+
+  componentDidMount = async () => {
+    const hotLoaderDisabled = zeppelinSolidityHotLoaderOptions.disabled;
+ 
+    try {
+      /// [Todo]:
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  renderLoader() {
+    return (
+      <div className={styles.loader}>
+        <Loader size="80px" color="red" />
+        <h3> Loading Web3, accounts, and contract...</h3>
+        <p> Unlock your metamask </p>
+      </div>
+    );
+  }
+
+  renderDeployCheck(instructionsKey) {
+    return (
+      <div className={styles.setup}>
+        <div className={styles.notice}>
+          Your <b> contracts are not deployed</b> in this network. Two potential reasons: <br />
+          <p>
+            Maybe you are in the wrong network? Point Metamask to localhost.<br />
+            You contract is not deployed. Follow the instructions below.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  renderPublish() {
+    return (
+      <div className={styles.wrapper}>
+        <Publish />
+      </div>
+    );
+  }
+
+  renderMyPhotos() {
+    return (
+      <div className={styles.wrapper}>
+        <MyPhotos />
+      </div>
+    );
+  }
+
+  renderPhotoMarketPlace() {
+    return (
+      <div className={styles.wrapper}>
+        <PhotoMarketplace />
+      </div>    
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+      <div className={styles.App}>
+        <Header />
+          {this.state.route === 'publish' && this.renderPublish()}
+          {this.state.route === 'my-photos' && this.renderMyPhotos()}
+          {this.state.route === 'photo-marketplace' && this.renderPhotoMarketPlace()}
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
