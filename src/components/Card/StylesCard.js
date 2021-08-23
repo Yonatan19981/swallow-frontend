cf0bd9327bf7210749fe7aa504a924814bf755be
YONATAN10111 <60484385+YONATAN10111@users.noreply.github.com>
2021-08-23 17:21:42 +0300

    Updated UI

---
+++ src/components/Card/StylesCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from 'semantic-ui-react';
+
+export const StyledCardOption = styled(Card)({
+    color: 'red!important'
+});
